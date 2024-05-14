5540ebccabf792c4c019470577fe39d6c1c65072
Namfon <varisa.vb@hotmail.com>
2024-05-14 12:43:39 +0700

    ex-6

--- ex-6.js
+++ ex-6.js
@@ -1,6 +1,6 @@
 //Start coding here
 const fruits = ["apple", "banana", "orange"];
 
-console.log("First Fruit: " + fruits[1]);
-console.log("Second Fruit: " + fruit[2]);
-console.log("Third Fruit: " + fruits[3]);
+console.log("First Fruit: " + fruits[0]); //change index from 1 to 0
+console.log("Second Fruit: " + fruits[1]); //edit array name from fruit to fruits & change index from 2 to 1
+console.log("Third Fruit: " + fruits[2]); //change index from 3 to 2
