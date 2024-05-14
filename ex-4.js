f3b12448a72641ee0eb5727d3ae4e05b7fb7cdef
Namfon <varisa.vb@hotmail.com>
2024-05-14 12:37:57 +0700

    ex-4

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,15 @@
 // Start coding here
 let calculator = {};
+
+calculator.add = (a, b) => a + b;
+calculator.subtract = (a, b) => a - b;
+calculator.multiply = (a, b) => a * b;
+calculator.divide = (a, b) => a / b;
+
+// console.log(calculator);
+
+let addResult = calculator.add(10, 20);
+console.log(addResult);
+
+let divideResult = calculator.divide(3000, 10);
+console.log(divideResult);
